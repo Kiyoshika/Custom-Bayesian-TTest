863f24ddfea3d93c1ed2fc0fd6e9620f31f02869
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2021-11-23 12:43:14 -0500

    Update CustomBayesTTest.R

--- CustomBayesTTest.R
+++ CustomBayesTTest.R
@@ -3,33 +3,68 @@
 # original bayes factor t-test from the BayesFactor package
 ####################################################################################
 
-# install.packages("BayesFactor")
+#' @title Bayesian Alternative Model
+#' 
+#' @description The general form of the alternative model, contrary to the null model.
+#' 
+#' @param t The value of your observed test statistic T = t
+#' @param standard_error_variance The variance of the standard error. In the Gaussian context of comparing two means, the standard error would be sqrt((s_x^2 + s_y^2)/n) (assuming an equal sample size of n). Thus, the variance would just be (s_x^2 + s_y^2) / n
+#'
+#' @returns The likelihood of the observed test statistic T = t under the alternative model. This can be used to obtain the bayes factor by dividing against the likelihood under the null model
+alt_model = function(t, standard_error_variance)
+{
+  return(exp(-(t^2)/(4*standard_error_variance)) / (2*sqrt(3.14159)*sqrt(standard_error_variance)))
+}
+
+
+
+#' @title Bayesian Null Model
+#' 
+#' @description The (asymptotic) sampling distribution of the test statistic given the null hypothesis is true.
+#' 
+#' @param t The value of your observed test statistic T = t
+#' @param standard_error_variance The variance of the standard error. In the Gaussian context of comparing two means, the standard error would be sqrt((s_x^2 + s_y^2)/n) (assuming an equal sample size of n). Thus, the variance would just be (s_x^2 + s_y^2) / n.
+#' 
+#' @returns The likelihood of the observed test statistic T = t under the null model. This is the denominator when computing the bayes factor.
+null_model = function(t, standard_error_variance)
+{
+  return(dnorm(t, mean = 0, sd = sqrt(standard_error_variance)))
+}
+
+
+
+#' @title Bayesian T-Test
+#' 
+#' @description This is a bayesian variant of the t-test which mergest frequentist and bayesian ideas together. We establish the null model as the asymptotic distribution of the test statistic given the null hypothesis is true (so far this is the same as frequentist statistics). However, when we establish the alternative model, we allow the mean to vary according to the standard error, i.e, the prior distribution of the mean is centered at zero and varies according to the standard error of the test stistic. The intuition behind this is that when the standard error grows, the variation in the effect size will also grow and vice versa when it shrinks. Thus, we make the variation of the mean to be dependent on the standard error.
+#' 
+#' @param g1 Group 1
+#' @param g2 Group 2 (assuming equal sample size to Group 1)
+#' 
+#' @returns The bayes factor which can be used to construct a decision rule, such as if bayes factor is > 3, then we "reject" the null model
+bayes_ttest = function(g1, g2)
+{
+  # general variance assuming g1 and g2 are independent
+  standard_error_variance = (var(g1) + var(g2)) / length(g1)
+  t = mean(g1) - mean(g2)
+  return(alt_model(t, standard_error_variance)/null_model(t, standard_error_variance))
+}
+
+
+
+# simulations (experiment with different means, variances, sample sizes)
 library(BayesFactor)
 
-trad_tt = c() # traditional t-test
-bf_cust = c() # my custom bayesian test
-bf_orig = c() # the original bayesian test from BayesFactor package
-
-# change the values of the mean to make the null hypothesis true/false
-
-# From my observations, the bayesian test(s) are pretty robust to false positives
-# and provide a good balance of true negatives and true positives (contrary to choosing
-# a 5% or 1% alpha, where 5% alphas can have higher false positives and 1% alphas can lack power
-# in small-sample-moderate-variance cases like below)
-for (i in 1:10000)
+trad_tt = c()
+bf_cust = c()
+bf_orig = c()
+for (i in 1:1000)
 {
-  x = rnorm(60, mean = 2, sd = 2)
-  y = rnorm(60, mean = 3, sd = 2)
+  x = rnorm(30, mean = 2, sd = 2)
+  y = rnorm(30, mean = 3, sd = 2)
   trad_tt[i] = ifelse(t.test(x,y)$p.value < 0.01, 1, 0)
   
-  # custom bayes factor test (specific to this standard error (e.g sample size 60 and total variance of 8))
-  # if you change the sample size and/or variance, you MUST re-derive the alternate model!
-  # perhaps in the future I will derive a general form for this and create a function
-  tstat = mean(x) - mean(y)
-  null_model = dnorm(tstat, mean = 0, sd = sqrt(8/60))
-  alt_model = (sqrt(15)*exp((-15*tstat^2)/8)) / (2^(3/2)*sqrt(3.14159))
-  
-  bf_cust[i] = ifelse(alt_model/null_model > 3, 1, 0)
+  # custom bayes factor test
+  bf_cust[i] = ifelse(bayes_ttest(x, y) > 3, 1, 0)
   
   # R bayes factor test
   bf_orig[i] = ifelse(extractBF(ttestBF(x,y))$bf > 3, 1, 0)
